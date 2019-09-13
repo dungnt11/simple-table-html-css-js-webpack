5f718a65810f8706a97b30e1e366a86ce1cfaf78
dung nguyen <nihilism.core@gmail.com>
2019-09-13 21:59:09 +0700

    build webpack babel and sass-loader

---
+++ webpack.config.js
@@ -0,0 +1,84 @@
+// Imports
+const path = require("path");
+const htmlWebpackPlugin = require("html-webpack-plugin");
+require("babel-register");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+const config = {
+  entry: "./js/index.js",
+
+  output: {
+    path: path.resolve(__dirname, "./dist"),
+    filename: "bundle.js"
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"]
+      },
+
+      {
+        test: /\.(css|sass|scss)$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader",
+            options: {
+              importLoaders: 2,
+              sourceMap: true
+            }
+          },
+          {
+            loader: "postcss-loader",
+            options: {
+              plugins: () => [require("autoprefixer")],
+              sourceMap: true
+            }
+          },
+          {
+            loader: "sass-loader",
+            options: {
+              sourceMap: true
+            }
+          }
+        ]
+      }
+    ]
+  },
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        js: {
+          test: /\.js$/,
+          name: "commons",
+          chunks: "all",
+          minChunks: 7
+        },
+        css: {
+          test: /\.(css|sass|scss)$/,
+          name: "commons",
+          chunks: "all",
+          minChunks: 2
+        }
+      }
+    }
+  },
+
+  plugins: [
+    new htmlWebpackPlugin({
+      template: "./index.html",
+      filename: "index.html",
+      hash: true
+    }),
+    new MiniCssExtractPlugin({
+      filename: "dist/[name].css"
+    })
+  ],
+  devServer: {
+    port: 3000
+  }
+};
+module.exports = config;
